c1e369386f5f02c6f6a27a62a6bfb1224a721a1f
Andrew Jones <thebinarysearchtree@gmail.com>
2022-10-23 19:52:42 +1000

    pcre2

--- extensions/pcre2.js
+++ extensions/pcre2.js
@@ -34,8 +34,8 @@ const installPcre2 = async () => {
   const filenames = await readdir(new URL('.', import.meta.url));
   if (!filenames.includes('Makefile')) {
     exec('./configure --enable-jit');
-    exec('sudo make install');
   }
+  exec('sudo make install');
 }
 
 await getFiles();
