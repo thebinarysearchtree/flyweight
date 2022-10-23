380da71c43628261c74d697492eae40e3283bdb7
Andrew Jones <thebinarysearchtree@gmail.com>
2022-10-23 17:49:43 +1000

    make extensions

---
+++ extensions/pcre2.js
@@ -0,0 +1,41 @@
+import { execSync as exec } from 'child_process';
+import fetch from 'node-fetch';
+import { readdir } from 'fs/promises';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
+const download = async (url) => {
+  const res = await fetch(url);
+  const filename = url.split('/').at(-1);
+  const file = createWriteStream(filename);
+  await pipeline(res.body, file);
+}
+
+const sqlite = 'sqlite-amalgamation-3390400';
+const pcre2 = 'pcre2-10.40';
+
+const getFiles = async () => {
+  const filenames = await readdir(new URL('.', import.meta.url));
+  if (!filenames.includes(sqlite)) {
+    await download('https://www.sqlite.org/2022/sqlite-amalgamation-3390400.zip');
+    exec(`unzip ${sqlite}.zip`);
+    exec(`rm ${sqlite}.zip`);
+  }
+  if (!filenames.includes(pcre2)) {
+    await download('https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.zip');
+    exec(`unzip ${pcre2}.zip`);
+    exec(`rm ${pcre2}.zip`);
+  }
+}
+
+const installPcre2 = async () => {
+  exec(`cd ${pcre2}`);
+  const filenames = await readdir(new URL('.', import.meta.url));
+  if (!filenames.includes('Makefile')) {
+    exec('./configure --enable-jit');
+    exec('make install');
+  }
+}
+
+await getFiles();
+await installPcre2();
