ab2770b074fb0cefab33c8a2f55fcbf42b93dce2
Andrew Jones <thebinarysearchtree@gmail.com>
2022-11-18 13:13:48 +1000

    use native fetch

--- extensions/pcre2.js
+++ extensions/pcre2.js
@@ -1,5 +1,4 @@
 import { execSync } from 'child_process';
-import fetch from 'node-fetch';
 import { readdir } from 'fs/promises';
 import { createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
