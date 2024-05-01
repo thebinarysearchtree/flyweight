9d53c04ac418c20068b1f8585a40e3bf9919841c
Andrew Jones <thebinarysearchtree@gmail.com>
2024-05-01 11:46:30 +1000

    export d1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Database from './src/db.js';
 import SQLiteDatabase from './src/sqlite.js';
+import D1Database from './src/d1.js';
 import { not, gt, gte, lt, lte, like } from './src/modifiers.js';
 
 export {
   Database,
   SQLiteDatabase,
+  D1Database,
   not,
   gt,
   gte,
