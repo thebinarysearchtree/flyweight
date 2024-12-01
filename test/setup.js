76ef69b3466759640c783f534dfad6b30e9d57e4
Andrew Jones <thebinarysearchtree@gmail.com>
2024-12-01 15:31:10 +1000

    update test database url

--- test/setup.js
+++ test/setup.js
@@ -8,7 +8,7 @@ await mkdir(folder, { recursive: true });
 const filenames = await readdir(folder);
 if (!filenames.includes('test.db')) {
   console.log('Downloading database.');
-  const res = await fetch('https://github.com/thebinarysearchtree/flyweight/raw/4b24a1882de3deb8077ef1333310cfde939c3149/test.db');
+  const res = await fetch('https://github.com/thebinarysearchtree/flyweight/raw/d8fcfc60171cb47bffffd0dc860a7265e4054181/test/databases/test.db');
   const path = join(folder.pathname, 'test.db');
   const file = createWriteStream(path);
   await pipeline(res.body, file);
