ec1fe3da52bc874596b0bb518e1ae61f3edbdda9
Andrew Jones <thebinarysearchtree@gmail.com>
2024-12-29 14:07:04 +1000

    remove flyweight-sqlite

--- test/db.js
+++ test/db.js
@@ -1,5 +1,37 @@
 import { SQLiteDatabase } from '../index.js';
-import adaptor from 'flyweight-sqlite';
+import sqlite3 from 'sqlite3';
+import { readFile, writeFile, readdir } from 'fs/promises';
+
+const readSql = async (path) => {
+  let sql = '';
+  if (path.endsWith('.sql')) {
+    sql = await readFile(path, 'utf8');
+  }
+  else {
+    const names = await readdir(path);
+    for (const name of names) {
+      if (name.endsWith('.sql')) {
+        let text = await readFile(join(path, name), 'utf8');
+        text = text.trim();
+        if (!text.endsWith(';')) {
+          text += ';';
+        }
+        text += '\n\n';
+        sql += text;
+      }
+    }
+  }
+  return sql.trim() + '\n';
+}
+
+const adaptor = {
+  sqlite3,
+  readFile,
+  writeFile,
+  readdir,
+  join,
+  readSql
+};
 
 const path = (subPath) => {
   const url = new URL(subPath, import.meta.url);
