afbf20a44571b1f836a5ff3151c03fd2a63f6ff5
Andrew Jones <thebinarysearchtree@gmail.com>
2025-06-19 09:31:30 +1000

    types

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -40,7 +40,7 @@ interface VirtualKeywords<T> {
   offset?: number;
 }
 
-interface Highlight<T> extends VirtualKeywords<T> {
+interface Highlighter<T> extends VirtualKeywords<T> {
   highlight: { column: keyof T, tags: [string, string] };
 }
 
@@ -48,7 +48,7 @@ interface Snippet<T> extends VirtualKeywords<T> {
   snippet: { column: keyof T, tags: [string, string], trailing: string, tokens: number };
 }
 
-interface HighlightQuery<W, T> extends Highlight<T> {
+interface HighlightQuery<W, T> extends Highlighter<T> {
   where?: W;
 }
 
