c672d5cbb11e577613421009b55422c95b0669dc
Andrew Jones <thebinarysearchtree@gmail.com>
2025-07-01 19:22:42 +1000

    bugs

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -17,7 +17,7 @@ type ObjectFunction = {
 }
 
 type MergeIncludes<T, U extends ObjectFunction> = 
-  T & { [K in keyof U]: ReturnType<U[K]> extends Promise<infer R> ? (R extends string | number | boolean | Date ? R | null : R) : never;
+  T & { [K in keyof U]: ReturnType<U[K]> extends Promise<infer R> ? (R extends any[] ? R : R | null) : never;
 };
 
 type ReturnTypes<T extends ObjectFunction> = {
